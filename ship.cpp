84f9cd077075115c6b408e89d13837fb4e4a42fd
Daniel.Aldridge <daniel.aldridge@dynetics.com>
2019-04-30 16:21:20 -0400

    Restructure

--- ship.cpp
+++
@@ -1,9 +0,0 @@
-#include "ship.h"
-#include <iostream>
-
-
-void Ship::ship_id(){
-	std::cout << "Ship name: " << name << std::endl;
-	std::cout << "The ship classification is: " << classification << std::endl;
-	std::cout << "The ship's armaments are: " << armaments << std::endl;
-}
